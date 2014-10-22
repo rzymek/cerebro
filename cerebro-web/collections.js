cb1e9c8d73ff8ef37a32184e384ec21670a28b7c
rzymek <rzymek@gmail.com>
2014-10-22 18:25:16 +0200

    restrict access

--- cerebro-web/collections.js
+++ cerebro-web/collections.js
@@ -1,15 +1,31 @@
 Probes = new Mongo.Collection('probes');
 Tracks = new Mongo.Collection('tracks');
 
-Probes.allow({
-    insert: function(userId, probe) {
-        probe.color = randomColor();
-        return true;
-    },
-    update: function() {
-        return true;
-    },
-    remove: function() {
-        return true;
-    }
-});
+if (Meteor.isServer) {
+    Probes.allow({
+        insert: function(userId, probe) {
+            probe.color = randomColor();
+            return true;
+        },
+        update: function() {
+            return true;
+        },
+        remove: function() {
+            return true;
+        }
+    });
+
+    Meteor.publish('probes', function() {
+        if (!this.userId) {
+            this.ready();
+            return;
+        }
+        var user = Meteor.users.findOne(this.userId);
+        var email = user && _.chain(user.emails).pluck('address').head().value();
+        if (email !== 'rzymek@gmail.com') {
+            this.ready();
+            return;
+        }
+        return Probes.find();
+    });
+}
