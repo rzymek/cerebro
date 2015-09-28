02d957223ffe47151b153d0145905ab005fd4890
rzymek <rzymek@gmail.com>
2015-09-28 21:11:04 +0200

    cleanup. rm old attic

--- attic/cerebro-web/common.js
+++
@@ -1,9 +0,0 @@
-Reports = new Mongo.Collection('reports');
-Senders = new Mongo.Collection('senders', {
-    transform: function(doc){
-        doc.display = function() {
-            return doc.name || doc._id;
-        }
-        return doc;
-    }
-});
--- attic/cerebro-web/sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-url=localhost:3000
-url=bullhorn.meteor.com
-curl $url/reset
-for i in {1..10};do
-    curl -w'\n' $url/report -d '{"sender":"'$i'", "listeners":["a","b","c"], "position":{"lat":52.'$i',"lon":24.'$i'}}'
-done
-
